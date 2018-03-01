30a91e5a179971b84edde0dd1f55687858dc438c
Roshni Kutty <roshni.ajkutty@gmail.com>
2018-03-01 11:56:14 -0500

    add and wire a Header component

--- client/components/app.js
+++ client/components/app.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import Header from './Header';
 
 export default App = () => {
-    return (<div>MarkUp from App.js</div>);
+    return (<div>
+        <Header />
+    </div>);
 }
