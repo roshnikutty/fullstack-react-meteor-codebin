d7393d04d420dbfaef8b4153197101270a3ceb4a
Roshni Kutty <roshni.ajkutty@gmail.com>
2018-03-02 12:27:45 -0500

    publication and subscription set

--- client/components/app.js
+++ client/components/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Header from './Header';
+import BinList from './bins/BinsList';
 
 export default App = () => {
     return (<div>
         <Header />
+        <BinList />
     </div>);
 }
